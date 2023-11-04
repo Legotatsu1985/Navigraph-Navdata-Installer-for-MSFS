f485d672b3f26ab469f44c6d524566dc8429f0e0
MENON1939 <ryotaro1985@outlook.com>
2023-11-04 19:52:28 +0900

    tesing file checker

--- test.py
+++ test.py
@@ -13,8 +13,5 @@ tkinter.Tk().withdraw()
 tkinter.messagebox.askokcancel('Navigraph Navdata Installer for PMDG 737NG','To use this installer, you need to download the latest AIRAC Navdata from SimPlaza. Select the archive file you downloaded in next dialog.')
 
 pmdg_nav_rar = filedialog.askopenfilename(filetypes=[('RAR Archive file','*.rar')], initialdir=os.path.abspath('.'), title="Select the latest Navigraph AIRAC archive file.(Navdata Installers)")
+print(pmdg_nav_rar)
 
-if pmdg_nav_rar == glob.glob(r"navigraph-navdata-installers-airac-cycle-*.rar"):
-    print("Valid")
-else:
-    print("Invalid")
