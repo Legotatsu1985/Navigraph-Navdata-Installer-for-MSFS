4636228baf06ba669810b91d42b5c3ded1a7f00b
MENON1939 <ryotaro1985@outlook.com>
2023-11-22 20:19:49 +0900

    Testing checkbox

--- test.py
+++ test.py
@@ -10,20 +10,32 @@ from tkinter import filedialog
 import rarfile
 
 def on_nav_update_select_button_click():
-    if msfs_native_update.get() == 1:
-        result_text = "It will update MSFS Native navdata."
-    if pmdg_update.get() == 1:
-        result_text = "It will update PMDG 737 navdata."
-    label.config(text=result_text)
+    checked = [msfs_native_update.get(), pmdg_update.get(), fenix_update.get()]
+    if checked == [1, 0, 0]:
+        label.config(text="MSFS Native navdata will be installed only.")
+    elif checked == [0, 1, 0]:
+        label.config(text="PMDG 737 navtdada will be installed only.")
+    elif checked == [0, 0, 1]:
+        label.config(text="Fenix A320 navdata will be installed only.")
+    elif checked == [1, 1, 0]:
+        label.config(text="MSFS Native and PMDG 737 navdatas will be installed.")
+    elif checked == [0, 1, 1]:
+        label.config(text="PMDG 737 and Fenix A320 navdatas will be installed.")
+    elif checked == [1, 0, 1]:
+        label.config(text="MSFS Native and Fenix A320 navdatas will be installed.")
+    else:
+        label.config(text="Please select the checkbox you want to install.")
 
 root = tkinter.Tk()
+root.title("Navigraph Navdata Installer for MSFS")
+root.geometry("800x600")
 msfs_native_update = tkinter.IntVar()
-msfs_check_box = tkinter.Checkbutton(root, text="MSFS Native Navdata", variable=msfs_native_update)
-msfs_check_box.pack()
 pmdg_update = tkinter.IntVar()
-pmdg_check_box = tkinter.Checkbutton(root, text="PMDG 737 Navdata", variable=pmdg_update)
-pmdg_check_box.pack()
-button = tkinter.Button(root, text="Update", command=on_nav_update_select_button_click)
+fenix_update = tkinter.IntVar()
+tkinter.Checkbutton(root, text="MSFS Native Navdata", variable=msfs_native_update).pack()
+tkinter.Checkbutton(root, text="PMDG 737 Navdata", variable=pmdg_update).pack()
+tkinter.Checkbutton(root, text="Fenix A320 Navdata", variable=fenix_update).pack()
+button = tkinter.Button(root, text="Start Update", command=on_nav_update_select_button_click)
 button.pack()
 label = tkinter.Label(root)
 label.pack()
