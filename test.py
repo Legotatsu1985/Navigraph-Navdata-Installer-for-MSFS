0f96e6d21c96bb8241851ac51e0bbfaf9a51344e
MENON1939 <ryotaro1985@outlook.com>
2023-12-01 11:32:11 +0900

    testing

--- test.py
+++ test.py
@@ -12,3 +12,7 @@ import rarfile
 f = open(r"C:\Users\Ryota\AppData\Roaming\Microsoft Flight Simulator\UserCfg.opt", "r")
 alltxt = f.readlines()
 f.close()
+MSFSpathL = len(alltxt)
+print(MSFSpathL)
+MSFSpathF = alltxt[MSFSpathL-1].strip()
+print(MSFSpathF)
