cdb1453d4ad928a3e1f2e2e3c7434dd20fd6408a
MENON1939 <ryotaro1985@outlook.com>
2023-12-01 11:53:50 +0900

    testing

--- main.py
+++ main.py
@@ -13,14 +13,17 @@ def get_msfs_opt_file():
         for msfs_opt_file in glob.glob(r'C:\Users\*\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\UserCfg.opt'):
             print("UserCfg.opt found " + msfs_opt_file + "(MS Store Version)")
             get_msfs_installed_path(msfs_opt_file)
+            return msfs_opt_file
     elif glob.glob(r'C:\Users\*\AppData\Roaming\Microsoft Flight Simulator\UserCfg.opt'):
         for msfs_opt_file in glob.glob(r'C:\Users\*\AppData\Roaming\Microsoft Flight Simulator\UserCfg.opt'):
             print("UserCfg.opt found " + msfs_opt_file + "(Steam Version)")
             get_msfs_installed_path(msfs_opt_file)
+            return msfs_opt_file
     else:
         tkinter.Tk().withdraw()
         tkinter.messagebox.showerror('Navigraph Navdata Installer','Cannot find the file "UserCfg.opt". Please select the folder contains "UserCfg.opt" file in next dialog.')
         msfs_opt_file = filedialog.askdirectory(initialdir=os.path.abspath('.'), title='Please select the folder contains "UserCfg.opt" file.')
+        return msfs_opt_file
 
 def get_msfs_installed_path(msfs_opt_file):
     f = open(msfs_opt_file, "r")
