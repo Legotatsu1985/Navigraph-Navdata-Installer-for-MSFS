2146b171e049086b1930843ee17103e86a97be9f
MENON1939 <ryotaro1985@outlook.com>
2023-12-01 14:27:03 +0900

    version text justify set to left

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import glob
+import linecache
 import os
 import shutil
 import sys
 import tkinter
 import tkinter.messagebox
 from tkinter import filedialog
-import linecache
 
 import rarfile
+
 
 def get_msfs_opt_file():
     if glob.glob(r'C:\Users\*\AppData\Local\Packages\Microsoft.FlightSimulator_8wekyb3d8bbwe\LocalCache\UserCfg.opt'):
@@ -407,9 +408,9 @@ fenix_checkbox = tkinter.IntVar()
 msfs_native_checkbutton = tkinter.Checkbutton(root, text="MSFS Native Navdata", variable=msfs_native_checkbox)
 pmdg_checkbutton = tkinter.Checkbutton(root, text="PMDG 737 Navdata", variable=pmdg_checkbox)
 fenix_checkbutton = tkinter.Checkbutton(root, text="Fenix A320 Navdata", variable=fenix_checkbox)
-msfs_native_nav_version = tkinter.Label(root)
-pmdg_nav_version = tkinter.Label(root)
-fenix_nav_verison = tkinter.Label(root)
+msfs_native_nav_version = tkinter.Label(root, justify="left")
+pmdg_nav_version = tkinter.Label(root, justify="left")
+fenix_nav_verison = tkinter.Label(root, justify="left")
 install_button = tkinter.Button(root, text="Install", command=on_nav_install_select_button_click)
 exit_button = tkinter.Button(root, text="Exit", command=sys.exit)
 info_label = tkinter.Label(root, text="Made by Legotatsu1985 with Tkinter", fg="blue", anchor=tkinter.S)
