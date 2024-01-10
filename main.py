0609c21bc44dbd24f2b7efdaffeaeed38217fb54
MENON1939 <ryotaro1985@outlook.com>
2024-01-10 15:06:35 +0900

    window size adjustments

--- main.py
+++ main.py
@@ -418,7 +418,7 @@ def fenix_install():
 
 root = tkinter.Tk()
 root.title("Navigraph Navdata Installer for MSFS")
-root.geometry("500x300")
+root.geometry("450x250")
 instruction_label = tkinter.Label(root, justify="center", text='[Select the checkbox you want to install, then press "Install".]')
 msfs_native_checkbox = tkinter.IntVar()
 pmdg_checkbox = tkinter.IntVar()
